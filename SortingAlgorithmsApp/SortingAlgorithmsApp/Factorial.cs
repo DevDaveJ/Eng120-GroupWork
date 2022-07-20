590cec9717a4eca9c142591473a494f04a21298a
STONELAP-2\david <dajoyce.br@gmail.com>
2022-07-20 12:06:37 +0100

    Recursive factorial exercise

---
+++ SortingAlgorithmsApp/SortingAlgorithmsApp/Factorial.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SortingAlgorithmsApp
+{
+    public class Factorial
+    {
+        public static int FactorialRecursive(int n)
+        {
+            if (n < 0) throw new ArgumentOutOfRangeException("Cannot calculate factorial of negative number");
+
+            return (n <= 1) ? 1 : n * FactorialRecursive(n - 1);
+        }
+    }
+}
