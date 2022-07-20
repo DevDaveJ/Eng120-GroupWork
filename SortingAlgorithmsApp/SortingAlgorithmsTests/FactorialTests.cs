2932ab68cd52902e1c69e0159f8e5ca28ed8987e
BuildTools <unconfigured@null.spigotmc.org>
2022-07-20 12:07:57 +0100

    add using sortingalgorithmsapp namespace to factorial tests

--- SortingAlgorithmsApp/SortingAlgorithmsTests/FactorialTests.cs
+++ SortingAlgorithmsApp/SortingAlgorithmsTests/FactorialTests.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using SortingAlgorithmsApp;
 namespace SortingAlgorithmsTests;
 
 public class FactorialTests
